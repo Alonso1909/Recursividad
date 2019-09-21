1a5758eaa8659efb9c6c44ee3e2a851732289695
Victor Manuel C.A <47401842+Alonso1909@users.noreply.github.com>
2019-09-20 22:25:27 -0500

    Al hacer pruebas con numeros negativos al momento de extraer la posicion del arreglo como el resultado del numero retornado era en negativo el recorrido lo hacia a la inversa, asi que lo solucione con una multiplicacion por '-1' para obtener su posicion en positivo.

--- NumaLtr.py
+++ NumaLtr.py
@@ -10,12 +10,12 @@ def NumaLtr(num): #Definomos nombre de metodo y variables a las
     if (num < 10 and num >= 0): #Caso base (para positivos)
         return Pos , tex[num]
     elif (num > -10 and num < 0): #Caso base (para negativos)
-        return Neg , tex[num]
+        return Neg , tex[num * -1]
     else:
         if num < 0: #Caso recursivo negativo
-            return NumaLtr(int(num / 10)), tex[num % -10]
+            return NumaLtr(int(num / 10)), tex[num % -10 * -1]
         else: #Caso recursivo positivo
             return NumaLtr(int(num / 10)), tex[num % 10]
 
-NumaLtr = NumaLtr(0)
+NumaLtr = NumaLtr(-123)
 print(NumaLtr)
