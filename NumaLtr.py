6d3985e3a5152881432713b9443006106ca1e48f
Victor Manuel C.A <47401842+Alonso1909@users.noreply.github.com>
2019-09-20 21:48:59 -0500

    Al agregar un caso base negativo y realizar pruebas con numero negativos surgio un nuevo problema el las divisiones ya que solo aplica para positivos, asi que agrege nuevas condiciones a los casos base y un nuevo caso recursivo negativo para la resolucion del problema de la diviciones.

--- NumaLtr.py
+++ NumaLtr.py
@@ -1,11 +1,16 @@
 def NumaLtr(num): #Definomos nombre de metodo en base a la operacion a realizar
+    Pos = 'Mas'
+    Neg = 'Menos'
 
-    if (num < 10): #Caso base (para positivos)
-        return num
-    elif (num > -10): #Caso base (para negativos)
-        return num
+    if (num < 10 and num >= 0): #Caso base (para positivos)
+        return Pos , num
+    elif (num > -10 and num < 0): #Caso base (para negativos)
+        return Neg , num
     else:
-        return NumaLtr(int(num / 10)), num % 10 #Caso recursivo
+        if num < 0:
+            return NumaLtr(int(num / 10)), num % -10 #Caso recursivo
+        else:
+            return NumaLtr(int(num / 10)), num % 10 #Caso recursivo
 
-NumaLtr = NumaLtr(123)
+NumaLtr = NumaLtr(-123)
 print(NumaLtr)
