9b6f2690babaae1e887741aea2344fc9d6f6e71f
Victor Manuel C.A <47401842+Alonso1909@users.noreply.github.com>
2019-09-20 22:06:54 -0500

    Asignamos una nueva variable a la que le asinamos un arreglo donde añadimos en texto los nombres de los numeros que componen nuestra base decimal (0 a 9), esto para pode realizar la conversion de numero a numero en texto, y lo implementamos en los casos bases y recursivos de forma que extraigan la posicion de acuerdo al numero retornado ya que estos son los mismo del 1 al 9.

--- NumaLtr.py
+++ NumaLtr.py
@@ -1,16 +1,21 @@
-def NumaLtr(num): #Definomos nombre de metodo en base a la operacion a realizar
+def NumaLtr(num): #Definomos nombre de metodo y variables a las
+    # que les asignamos lo que nesecitamos, los nombres en base
+    # a la operacion a realizar o a lo que almacenaran
+
+    tex = ['Cero','Uno','Dos','Tres','Cuatro','Cinco',
+           'Seis','Siete','Ocho','Nueve']
     Pos = 'Mas'
     Neg = 'Menos'
 
     if (num < 10 and num >= 0): #Caso base (para positivos)
-        return Pos , num
+        return Pos , tex[num]
     elif (num > -10 and num < 0): #Caso base (para negativos)
-        return Neg , num
+        return Neg , tex[num]
     else:
-        if num < 0:
-            return NumaLtr(int(num / 10)), num % -10 #Caso recursivo
-        else:
-            return NumaLtr(int(num / 10)), num % 10 #Caso recursivo
+        if num < 0: #Caso recursivo negativo
+            return NumaLtr(int(num / 10)), tex[num % -10]
+        else: #Caso recursivo positivo
+            return NumaLtr(int(num / 10)), tex[num % 10]
 
-NumaLtr = NumaLtr(-123)
+NumaLtr = NumaLtr(0)
 print(NumaLtr)
