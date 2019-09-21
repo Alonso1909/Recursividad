2315d383126b12646aaa044125ea63b3efa3251f
Victor Manuel C.A <47401842+Alonso1909@users.noreply.github.com>
2019-09-20 21:16:30 -0500

    Se agrego un caso base para numeros negativos ya que ocupa otras modificaciones diferentes al caso base para positivos.

--- NumaLtr.py
+++ NumaLtr.py
@@ -1,6 +1,8 @@
 def NumaLtr(num): #Definomos nombre de metodo en base a la operacion a realizar
 
-    if (num < 10): #Caso base
+    if (num < 10): #Caso base (para positivos)
+        return num
+    elif (num > -10): #Caso base (para negativos)
         return num
     else:
         return NumaLtr(int(num / 10)), num % 10 #Caso recursivo
