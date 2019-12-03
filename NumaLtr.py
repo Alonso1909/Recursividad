f8ce1b01b0f7561466b3885a30abb676f456d2a0
Victor Manuel C.A <47401842+Alonso1909@users.noreply.github.com>
2019-12-02 19:37:21 -0600

    Se modifico nuestro codigo de forma tal que pasara de ser un algoritmo estructurado a uno orientado a objetos. Ademas se corriguieron algunos desperfectos.

--- NumaLtr.py
+++ NumaLtr.py
@@ -1,21 +1,33 @@
-def NumaLtr(num): #Definomos nombre de metodo y variables a las
-    # que les asignamos lo que nesecitamos, los nombres en base
-    # a la operacion a realizar o a lo que almacenaran
+# Autor:: Victor Manuel Cervantes Alonso 18930195
 
-    tex = ['Cero','Uno','Dos','Tres','Cuatro','Cinco',
-           'Seis','Siete','Ocho','Nueve']
-    Pos = 'Mas'
-    Neg = 'Menos'
+class ConversorNumLtr:#Definomos nombre de la clase y variables a las
+        # que les asignamos lo que nesecitamos, los nombres en base
+        # a la operacion a realizar o a lo que almacenaran
+    __tex = ['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco',
+           'Seis', 'Siete', 'Ocho', 'Nueve']
+    __Pos = 'Mas'
+    __Neg = 'Menos'
+    __auxBP = str
+    __auxBN = str
+    __auxRP = str
+    __auxRN = str
+    __ret = str
 
-    if (num < 10 and num >= 0): #Caso base (para positivos)
-        return Pos , tex[num]
-    elif (num > -10 and num < 0): #Caso base (para negativos)
-        return Neg , tex[num * -1]
-    else:
-        if num < 0: #Caso recursivo negativo
-            return NumaLtr(int(num / 10)), tex[num % -10 * -1]
-        else: #Caso recursivo positivo
-            return NumaLtr(int(num / 10)), tex[num % 10]
+    def NumaLtr(self,num): #Definomos nombre de metodo y la varible de
+        # entrada que se ocupara en todos los procesos
+        if (num < 10 and num >= 0): #Caso base (para positivos)
+            self.__casoBP = self.__Pos + ' : ' + self.__tex[num]
+            return self.__casoBP
+        elif (num > -10 and num < 0): #Caso base (para negativos)
+            self.__auxBN = self.__Neg + ' : ' + self.__tex[num * -1]
+            return self.__auxBN
+        else:
+            if num < 0: #Caso recursivo negativo
+                self.__auxRN = self.NumaLtr(int(num / 10)) + ', ' + self.__tex[num % -10 * -1]
+                return self.__auxRN
+            else: #Caso recursivo positivo
+                self.__auxRP = self.NumaLtr(int(num / 10)) + ', ' + self.__tex[num % 10]
+                return self.__auxRP
 
-NumaLtr = NumaLtr(-123)
-print(NumaLtr)
+NumLtr = ConversorNumLtr()
+print(NumLtr.NumaLtr(123))
